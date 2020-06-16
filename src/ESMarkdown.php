c8eebcbb5f59407a9205ab7dd4a488aa0ceae7e5
Josh Bruce <josh@8fold.pro>
2020-06-16 15:35:42 -0500

    Update ESMarkdown.php

--- src/ESMarkdown.php
+++ src/ESMarkdown.php
@@ -53,6 +53,7 @@ class ESMarkdown implements Shooped
     {
         $content = $this->content($markdownReplacements, $caseSensitive)
             ->unfold();
+        $cap = UIKit::p("Hello");
         $html = UIKit::markdown($content)->unfold();
         $html = Shoop::string($html)
             ->replace($htmlReplacements, $caseSensitive);
