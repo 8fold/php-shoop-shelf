929e869610cd6d1295ce2f3f23bb0228becd06be
Josh Bruce <josh@8fold.pro>
2020-06-16 19:46:32 -0500

    deprecate using markup

--- src/ESMarkdown.php
+++ src/ESMarkdown.php
@@ -4,6 +4,12 @@ namespace Eightfold\ShoopExtras;
 
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
+use League\CommonMark\Converter;
+use League\CommonMark\DocParser;
+use League\CommonMark\Environment;
+use League\CommonMark\HtmlRenderer;
+use League\CommonMark\Extension\Table\TableExtension;
+
 use Eightfold\Shoop\Helpers\Type;
 use Eightfold\Shoop\Interfaces\Shooped;
 use Eightfold\Shoop\Traits\ShoopedImp;
@@ -48,12 +54,24 @@ class ESMarkdown implements Shooped
         $markdownReplacements = [],
         $htmlReplacements = [],
         $caseSensitive = true,
-        $minified = true
+        $minified = true,
+        $config = [
+            'html_input' => 'strip',
+            'allow_unsafe_links' => false,
+        ]
     )
     {
         $content = $this->content($markdownReplacements, $caseSensitive)
             ->unfold();
-        $html = UIKit::markdown($content)->unfold();
+
+        $environment = Environment::createCommonMarkEnvironment()
+            ->addExtension(new TableExtension());
+
+        $parser = new DocParser($environment);
+        $renderer = new HtmlRenderer($environment);
+
+        $html = (new Converter($parser, $renderer))
+            ->convertToHtml($content);
         $html = Shoop::string($html)
             ->replace($htmlReplacements, $caseSensitive);
         if ($minified) {
