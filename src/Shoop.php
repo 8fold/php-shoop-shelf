7519adcbad841c9e2eaf5b1baddba5460af64ee1
Josh Bruce <josh@8fold.pro>
2020-07-02 14:11:45 -0500

    ESUri initial

--- src/Shoop.php
+++ src/Shoop.php
@@ -4,24 +4,32 @@ namespace Eightfold\ShoopExtras;
 
 use Eightfold\Shoop\Shoop as EFShoop;
 
-use Eightfold\ShoopExtras\ESStore;
-use Eightfold\ShoopExtras\ESMarkdown;
-use Eightfold\ShoopExtras\ESPath;
+use Eightfold\ShoopExtras\{
+    ESStore,
+    ESMarkdown,
+    ESPath,
+    ESUri
+};
 
 class Shoop extends EFShoop
 {
-    static public function store($path): ESStore
-    {
-        return ESStore::fold($path);
-    }
-
     static public function markdown($content): ESMarkdown
     {
         return ESMarkdown::fold($content);
     }
 
+    static public function store($path): ESStore
+    {
+        return ESStore::fold($path);
+    }
+
     static public function path($path): ESPath
     {
         return ESPath::fold($path);
+    }
+
+    static public function uri($uri)
+    {
+        return ESUri::fold($uri);
     }
 }
